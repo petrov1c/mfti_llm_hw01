7029b14aec06f241a8bacd11a37bb36ef3e27cc4
Петров Александр <petrov1c@yandex.ru>
2025-01-09 10:40:21 +0300

    Добавил модель для QA

--- src/model.py
+++ src/model.py
@@ -1,4 +1,11 @@
-from transformers import AutoModelForSequenceClassification, AutoTokenizer, TrainingArguments, Trainer
+from transformers import (
+    AutoModelForSequenceClassification,
+    AutoModelForQuestionAnswering,
+    AutoTokenizer,
+    TrainingArguments,
+    Trainer
+)
+
 from peft import LoraConfig, get_peft_model
 
 from src.config import Config
@@ -42,7 +49,6 @@ def create_trainer(config: Config, train_data, eval_data):
         output_dir=config.output_dir,
         eval_strategy='epoch',
         save_strategy='epoch',
-        # logging_steps=10,
         logging_dir='logs',
         per_device_train_batch_size=config.data_config.batch_size,
         per_device_eval_batch_size=config.data_config.batch_size,
@@ -69,8 +75,8 @@ def create_trainer(config: Config, train_data, eval_data):
 
 
 def create_trainer_with_adapter(config: Config, train_dataset, eval_dataset):
-    def add_lora_adapter(model, r=16, alpha=32):
-        config = LoraConfig(r=r, lora_alpha=alpha, task_type='CAUSAL_LM')
+    def add_lora_adapter(model, r=16, alpha=32, dropout=0.1):
+        config = LoraConfig(r=r, lora_alpha=alpha, lora_dropout=dropout, task_type='CAUSAL_LM')
         return get_peft_model(model, config)
 
     model = AutoModelForSequenceClassification.from_pretrained(
@@ -106,7 +112,7 @@ def create_trainer_with_adapter(config: Config, train_dataset, eval_dataset):
         load_best_model_at_end=True,
         metric_for_best_model=config.monitor_metric,
         report_to = 'clearml',
-        # disable_tqdm = True,
+        disable_tqdm = True,
     )
 
     trainer = Trainer(
@@ -120,3 +126,51 @@ def create_trainer_with_adapter(config: Config, train_dataset, eval_dataset):
     )
 
     return trainer
+
+
+def create_trainer_qa(config: Config, train_data, eval_data):
+    model = AutoModelForQuestionAnswering.from_pretrained(
+        config.model_kwargs['model_name'],
+        torch_dtype=config.model_kwargs['fmt'],
+    )
+
+    tokenizer = AutoTokenizer.from_pretrained(config.model_kwargs['model_name'])
+
+    if 'freeze' in config.model_kwargs and config.model_kwargs['freeze']:
+        for param in model.bert.embeddings.parameters():
+            param.requires_grad = False
+        for param in model.bert.encoder.layer[:int(config.model_kwargs['freeze'] * len(model.bert.encoder.layer) / 100)].parameters():
+            param.requires_grad = False
+
+    optimizer = load_object(config.optimizer)(
+        filter(lambda p: p.requires_grad, model.parameters()),
+        **config.optimizer_kwargs,
+    )
+
+    scheduler = load_object(config.scheduler)(optimizer, **config.scheduler_kwargs)
+
+    training_args = TrainingArguments(
+        output_dir=config.output_dir,
+        eval_strategy='epoch',
+        save_strategy='epoch',
+        logging_dir='logs',
+        per_device_train_batch_size=config.data_config.batch_size,
+        per_device_eval_batch_size=config.data_config.batch_size,
+        max_grad_norm=config.model_kwargs['max_grad_norm'],
+        num_train_epochs=config.n_epochs,
+        dataloader_num_workers=config.data_config.n_workers,
+        load_best_model_at_end=True,
+        metric_for_best_model=config.monitor_metric,
+        report_to='clearml',
+        # disable_tqdm=True,
+    )
+
+    return Trainer(
+        model=model,
+        tokenizer=tokenizer,
+        args=training_args,
+        train_dataset=train_data,
+        eval_dataset=eval_data,
+        optimizers=(optimizer, scheduler),
+        compute_metrics=compute_metrics,
+    )
